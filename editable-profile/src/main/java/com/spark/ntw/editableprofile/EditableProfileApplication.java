21bc2008bf39a591a066c7606bd1f2d8dafc7290
nkurup <natesh.kurup@gmail.com>
2018-11-20 00:05:00 +0530

    creating project structure

---
+++ editable-profile/src/main/java/com/spark/ntw/editableprofile/EditableProfileApplication.java
@@ -0,0 +1,12 @@
+package com.spark.ntw.editableprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditableProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EditableProfileApplication.class, args);
+	}
+}
